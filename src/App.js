d783311e958c726ca14558b47b799cd9508aa58f
Dorel Barbu <dorelbarbu96@gmail.com>
2018-03-12 12:46:54 +0200

    Added firebase authentication

--- src/App.js
+++ src/App.js
@@ -2,9 +2,23 @@ import React, { Component } from 'react';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import { View, Text } from 'react-native';
+import firebase from 'firebase';
 import reducers from './reducers';
 
 class App extends Component {
+
+    componentWillMount() {
+        var config = {
+            apiKey: "AIzaSyATiJc4IfbHH-OoWmFd-RTRyTxPy87t0t8",
+            authDomain: "log-in-app-6d1e5.firebaseapp.com",
+            databaseURL: "https://log-in-app-6d1e5.firebaseio.com",
+            projectId: "log-in-app-6d1e5",
+            storageBucket: "log-in-app-6d1e5.appspot.com",
+            messagingSenderId: "840042710478"
+        };
+        firebase.initializeApp(config);
+    }
+
     render() {
         return (
             <Provider store={createStore(reducers)}>
