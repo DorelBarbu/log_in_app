87098f46f8d6ff776ad726430c2c76c1e84edbda
Dorel Barbu <dorelbarbu96@gmail.com>
2018-03-21 15:19:03 +0200

    Added common components, reducers, actions, LoginForm

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { createStore } from 'redux';
 import { View, Text } from 'react-native';
 import firebase from 'firebase';
 import reducers from './reducers';
-
+import LoginForm from './components/LoginForm';
 class App extends Component {
 
     componentWillMount() {
@@ -23,9 +23,7 @@ class App extends Component {
         return (
             <Provider store={createStore(reducers)}>
                 <View>
-                    <Text>
-                        Hello
-                    </Text>
+                    <LoginForm />
                 </View>
             </Provider>
         );
